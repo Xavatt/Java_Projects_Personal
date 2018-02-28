f0d5c78e21c3635cb1551a0d39968d99fb808987
Xavier Guinto <xavierguinto3@outlook.es>
2018-02-27 21:37:27 -0600

    Se agrego un int y prueba de guardado de licencia

--- Bring.java
+++ Bring.java
@@ -3,5 +3,7 @@ public class Bring
   public static void main(String[] args)
   {
       System.out.println("Bring your best mode");
+      int a=2;
+      System.out.println("The value is: "+a);
   }
 }
