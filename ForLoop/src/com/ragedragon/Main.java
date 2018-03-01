3cc28a992101e876baa4775a38b4fd8db5676828
Xavier Guinto <xavierguinto3@outlook.es>
2018-03-01 16:00:59 -0600

    Primer avance

--- ForLoop/src/com/ragedragon/Main.java
+++ ForLoop/src/com/ragedragon/Main.java
@@ -4,6 +4,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-	    
+        System.out.println("10,000 at 2% interest = "+calculateInterest(10000,2.0));
+        System.out.println("10,000 at 3% interest = "+calculateInterest(10000,3.0));
+        System.out.println("10,000 at 4% interest = "+calculateInterest(10000,4.0));
+        System.out.println("10,000 at 5% interest = "+calculateInterest(10000,5.0));
+
+        for (int i=0; i < 5; i++)
+        {
+            System.out.println("Loop " + i + " Hello!");
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate)
+    {
+        return (amount * (interestRate/100));
     }
 }
