3ffde60c832954a2c1b32a93d26767ef0d0ed138
Xavier Guinto <xavierguinto3@outlook.es>
2018-03-02 20:12:54 -0600

    Se agrego dos for para realizar un loop

--- ForLoop/src/com/ragedragon/Main.java
+++ ForLoop/src/com/ragedragon/Main.java
@@ -13,6 +13,21 @@ public class Main
         {
             System.out.println("Loop " + i + " Hello!");
         }
+
+        for(int i =2; i < 9; i++)
+        {
+            System.out.println("10,000 at "+ i +" % interest = "+String.format("%.2f",calculateInterest(10000,i)));
+        }
+
+        System.out.println("\n");
+
+        for(int i =8; i >= 2 ; i--)
+        {
+            System.out.println("10,000 at "+ i +" % interest = "+String.format("%.2f",calculateInterest(10000,i)));
+        }
+
+
+
     }
 
     public static double calculateInterest(double amount, double interestRate)
