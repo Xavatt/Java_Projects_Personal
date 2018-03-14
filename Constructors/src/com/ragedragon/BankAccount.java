71c93e343c9476dfd3d6da9ee94caef4b435cbb3
Xavier Guinto <xavierguinto3@outlook.es>
2018-03-14 16:18:50 -0600

    Nuevo programa acerca de  constructores con uso de Getter's & Setter's

---
+++ Constructors/src/com/ragedragon/BankAccount.java
@@ -0,0 +1,97 @@
+package com.ragedragon;
+
+public class BankAccount
+{
+    private String number;
+    private double balance;
+    private String customerName;
+    private String phoneNumber;
+    private String email;
+
+    public BankAccount(String number, double balance, String customerName,String phoneNumber, String email)
+    {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Function to control the deposit of the balance of the account.
+    public void deposit(double depositAmount)
+    {
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+    // Function to control the withdrawal of the balance of the account.
+    public void withdrawal(double withdrawalAmount)
+    {
+        // Checks that the account has enough money for the withdrawal.
+        if(this.balance - withdrawalAmount < 0)
+        {
+            System.out.println("Only: " + balance + " available. Withdrawal not processed");
+        }
+        // If the account has enough money the Withdrawal proceeds.
+        else
+        {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = "+ this.balance);
+        }
+    }
+
+    /**
+     *  Getter's.
+     */
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    /**
+     *  Setter's.
+     */
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setCustomerName(String customerName)
+    {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
---
+++ Constructors/src/com/ragedragon/Main.java
@@ -0,0 +1,30 @@
+package com.ragedragon;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        BankAccount xavierAccount = new BankAccount("9029",0.00,"Xavier Guinto", "+52 55-32-76-76-43","xavierguinto3@outlook.es");
+        xavierAccount.setNumber("9029");
+        xavierAccount.setBalance(0.00);
+        xavierAccount.setCustomerName("Xavier Guinto");
+        xavierAccount.setEmail("xavier3@outlook.es");
+        xavierAccount.setPhoneNumber("+52 55-32-76-76-43");
+
+        System.out.println("The client information is");
+        System.out.println("Number: " + xavierAccount.getNumber());
+        System.out.println("Balance: "+ xavierAccount.getBalance());
+        System.out.println("Customer Name: "+ xavierAccount.getCustomerName());
+        System.out.println("Phone Number: "+ xavierAccount.getPhoneNumber());
+        System.out.println("Email: "+ xavierAccount.getEmail());
+
+
+        System.out.println("\n");
+        xavierAccount.deposit(50.0);
+        xavierAccount.withdrawal(100.0);
+        System.out.println("\n");
+        xavierAccount.deposit(51.0);
+        xavierAccount.withdrawal(100.0);
+    }
+}
