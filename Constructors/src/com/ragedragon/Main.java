03eab2bb053a8c3fa16090a3095da7a4dceacf7f
Xavier Guinto <xavierguinto3@outlook.es>
2018-03-15 22:39:30 -0600

    Se comentaron algunas lineas

--- Constructors/src/com/ragedragon/Main.java
+++ Constructors/src/com/ragedragon/Main.java
@@ -6,11 +6,11 @@ public class Main
     public static void main(String[] args)
     {
         BankAccount xavierAccount = new BankAccount("9029",0.00,"Xavier Guinto", "+52 55-32-76-76-43","xavierguinto3@outlook.es");
-        xavierAccount.setNumber("9029");
+       /* xavierAccount.setNumber("9029");
         xavierAccount.setBalance(0.00);
         xavierAccount.setCustomerName("Xavier Guinto");
         xavierAccount.setEmail("xavier3@outlook.es");
-        xavierAccount.setPhoneNumber("+52 55-32-76-76-43");
+        xavierAccount.setPhoneNumber("+52 55-32-76-76-43");*/
 
         System.out.println("The client information is");
         System.out.println("Number: " + xavierAccount.getNumber());
